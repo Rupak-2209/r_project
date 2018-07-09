113609eaa93546ef46caacab3dfbb81129c16877
Rupak-2209 <rupak@newput.com>
2018-07-10 00:32:53 +0530

    Changes in calendar using jQuery

--- jQueryCalendar/jQueryCalendar.js
+++ jQueryCalendar/jQueryCalendar.js
@@ -1,218 +1,234 @@
-var dateContainer = null;
-var date = null;
-var yearHeading = null;
-var monthHeading = null;
-var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thusday", "Friday", "Saturday"];
-var currentDate = new Date();
-var eventStartDate = 0;
-var eventEndDate = 0;
+var _dateContainer = null;
+var _date = null;
+var _yearHeading = null;
+var _monthHeading = null;
+var _monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var _day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thusday", "Friday", "Saturday"];
+var _currentDate = new Date();
+var _eventStartDate = 0;
+var _eventEndDate = 0;
+var _employees = {
+    "January": [
+        { "name":"Darshana", "birth":"1997-01-15" },
+        { "name":"Vipin Joshi", "birth":"1946-01-15" },
+        { "name":"Shubham Katariya", "birth":"1995-01-03" },
+        { "name":"Shikha Shakarwar", "birth":"1994-01-15" }
+    ],
+    "February": [
+    ],
+    "March": [
+    ],
+    "April": [
+        { "name":"Sonam Ravi Gupta", "birth":"1987-04-22" },
+        { "name":"Siyaram Patidar", "birth":"1985-04-03" },
+        { "name":"Gurpreet Chhabra", "birth":"1995-04-02" }
+    ],
+    "May": [
+        { "name":"Rahul Kulmi", "birth":"1988-05-28" },
+        { "name":"Mayur Vaidya", "birth":"1995-05-09" },
+        { "name":"Amit Nagar", "birth":"1986-05-10" },
+        { "name":"Deepak Patidar", "birth":"1990-05-10" }
+    ],
+    "June": [
+        { "name":"Vishal Patidar", "birth":"1994-06-20" }
+    ],
+    "July": [
+        { "name":"Awanish Tiwari", "birth":"1974-07-06" },
+        { "name":"Surendra Patidar", "birth":"1988-07-21" },
+        { "name":"Anjana Singh", "birth":"1992-07-24" }
+    ],
+    "August": [
+        { "name":"Aaditya Paliwal", "birth":"1994-08-08" }
+    ],
+    "September": [
+        { "name":"Rashmi Soni", "birth":"1993-09-19" },
+        { "name":"Rupak Gupta", "birth":"1995-09-22" }
+    ],
+    "October": [
+        { "name":"Piyush Chandal", "birth":"1996-10-16" },
+        { "name":"Varsha Tyagi", "birth":"1992-10-13" }
+    ],
+    "November": [
+        { "name":"Priyanshi Asawara", "birth":"1993-11-19" },
+        { "name":"Shashank Saxena", "birth":"1993-11-19" }
+    ],
+    "December": [
+      { "name":"Nitesh Thakur", "birth":"1990-12-12" },
+      { "name":"Satya Sir", "birth":"1983-12-12" },
+      { "name":"Abhijeet", "birth":"1996-12-03" }
+    ]
+}
 
-var employees = [
-    { "name":"Rahul Kulmi", "birth":"1988-05-28" },
-    { "name":"Vishal Patidar", "birth":"1994-06-20" },
-    { "name":"Awanish Tiwari", "birth":"1974-07-06" },
-    { "name":"Surendra Patidar", "birth":"1988-07-21" },
-    { "name":"Anjana Singh", "birth":"1992-07-24" },
-    { "name":"Aaditya Paliwal", "birth":"1994-08-08" },
-    { "name":"Varsha Tyagi", "birth":"1992-10-13" },
-    { "name":"Priyanshi Asawara", "birth":"1993-11-19" },
-    { "name":"Shashank Saxena", "birth":"1993-11-19" },
-    { "name":"Nitesh Thakur", "birth":"1990-12-12" },
-    { "name":"Satya Sir", "birth":"1983-12-12" },
-    { "name":"Shubham Katariya", "birth":"1995-01-03" },
-    { "name":"Shikha Shakarwar", "birth":"1994-01-15" },
-    { "name":"Vipin Joshi", "birth":"1946-01-15" },
-    { "name":"Gurpreet Chhabra", "birth":"1995-04-02" },
-    { "name":"Rashmi Soni", "birth":"1993-09-19" },
-    { "name":"Sonam Ravi Gupta", "birth":"1987-04-22" },
-    { "name":"Siyaram Patidar", "birth":"1985-04-03" },
-    { "name":"Shubham Choubey", "birth":"1993-05-08" },
-    { "name":"Mayur Vaidya", "birth":"1995-05-09" },
-    { "name":"Amit Nagar", "birth":"1986-05-10" },
-    { "name":"Deepak Patidar", "birth":"1990-05-10" },
-    { "name":"Rupak Gupta", "birth":"1995-09-22" },
-    { "name":"Piyush Chandal", "birth":"1996-10-16" },
-    { "name":"Darshana", "birth":"1997-01-15" },
-    { "name":"Abhijeet", "birth":"1996-12-03" }
-];
-
-(function() {
+$(document).ready(function(){
     createHeadings();
     createCalendar();
     addBirthdayEvent();
-})();
+});
 
 function addBirthdayEvent() {
     //fetch all birthday of this month
-    var first = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+    var firstDay = new Date(_currentDate.getFullYear(), _currentDate.getMonth(), 1);
     var names;
-    for(var i = 0; i < employees.length; i++) {
-        var person = new Date(employees[i].birth);
-        if(person.getMonth() === currentDate.getMonth()) {
-            var title = $(".date-block").eq(person.getDate() + first.getDay() - 1).children().attr("title");
+    console.log(_employees[_monthNames[firstDay.getMonth()]]);
+    for(var i = 0; i < _employees[_monthNames[firstDay.getMonth()]].length; i++) {
+        var person = new Date(_employees[_monthNames[firstDay.getMonth()]][i].birth);
+        if(person.getMonth() === _currentDate.getMonth()) {
+            var dateIndex = person.getDate() + firstDay.getDay() - 1;
+            var title = $(".date-block").eq(dateIndex).children(".material-icons").attr("title");
             if(title == null) {
-                $(".date-block").eq(person.getDate() + first.getDay() - 1).children().addClass("birthday").attr("title", employees[i].name);
+                $(".date-block").eq(dateIndex).append("<i class='material-icons'>cake</i>");
+                $(".date-block").eq(dateIndex).children(".material-icons").addClass("birthday").attr("title", _employees[_monthNames[firstDay.getMonth()]][i].name);
+                $(".date-block").eq(dateIndex).children(".material-icons").click(function(event) {
+                    event.stopPropagation();
+                    alert(event.target.title);
+                });
             } else {
-                names = title + "," + employees[i].name;
-                $(".date-block").eq(person.getDate() + first.getDay() - 1).children().attr("title", names);
+                names = title + "," + _employees[_monthNames[firstDay.getMonth()]][i].name;
+                $(".date-block").eq(dateIndex).children(".material-icons").addClass("birthday").attr("title", names);
             }
         }
     }
-    if(new Date().getMonth() == currentDate.getMonth()) {
-        $(".date-block").eq(new Date().getDate() + first.getDay() - 1).children().addClass("today").attr("title", "Today");
+    if(new Date().getMonth() == _currentDate.getMonth()) {
+        $(".date-block").eq(new Date().getDate() + firstDay.getDay() - 1).append("<i class='fa fa-sun-o sun'></i>").children(".sun").addClass("today").attr("title", "Today");
     }
 }
 
 function createHeadings() {
-    monthHeading = $("<div></div>").attr("class", "month-heading");
+    _monthHeading = $("<div></div>").addClass("month-heading");
     updateMonthHeading();
-    yearHeading = $("<div></div>").attr("class", "year-heading");
+    _yearHeading = $("<div></div>").addClass("year-heading");
     updateYearHeading();
-    weekDaysList = $("<div></div>").attr("class", "week-days-list");
+    weekDaysList = $("<div></div>").addClass("week-days-list");
     weekDay = $("<ul></ul>").addClass("day-name");
     for(var i = 0; i < 7; i++){
-        weekDay.append("<li class='days'>" +day[i]+ "</li>");
+        weekDay.append("<li class='days'>"+ _day[i] +"</li>");
     }
     weekDaysList.append(weekDay);
-    dateContainer = $("<div></div>").addClass("date-container");
-    $("#calendar").append(yearHeading, monthHeading, weekDaysList);
+    _dateContainer = $("<div></div>").addClass("date-container");
+    $("#calendar").append(_yearHeading, _monthHeading, weekDaysList);
 }
 
 function updateMonthHeading() {
-    monthHeading.html("<button onclick='previousMonth()' class='left-arrow'>&lt</button>"+ monthNames[currentDate.getMonth()] +"<button onclick='nextMonth()' class='right-arrow'>&gt</button>");
+    _monthHeading.html("<button onclick='previousMonth()' class='left-arrow'>&lt</button>"+ _monthNames[_currentDate.getMonth()] +"<button onclick='nextMonth()' class='right-arrow'>&gt</button>");
 }
 
 function updateYearHeading() {
-    yearHeading.html("<button onclick='previousYear()' class='left-arrow'>&lt</button>"+ currentDate.getFullYear() +"<button onclick='nextYear()' class='right-arrow'>&gt</button>");
+    _yearHeading.html("<button onclick='previousYear()' class='left-arrow'>&lt</button>"+ _currentDate.getFullYear() +"<button onclick='nextYear()' class='right-arrow'>&gt</button>");
 }
 
 function createCalendar() {
-    var year = currentDate.getFullYear();
-    var month = currentDate.getMonth();
+    var year = _currentDate.getFullYear();
+    var month = _currentDate.getMonth();
     var firstDay = new Date(year, month, 1);
     var lastDay = new Date(year, month + 1, 0);
     var previousMonth = new Date(year, month, 0);
     var previousDate = previousMonth.getDate();
-    var nextMonthDate = new Date(year, month + 1, 1);
-    var nextDate = nextMonthDate.getDate();
     var countNextDate = firstDay.getDay();
-
-    var temp = previousMonth.getDate() - countNextDate + 1;
-    var dateCount;
-    if((firstDay.getDay() + lastDay.getDate()) > 35) {
-        numOfDates = 42;
-    } else {
-        numOfDates = 35;
-      }
-    for(var count = 0; count < numOfDates; count++) {
-        dateCount = new Date(year, month - 1, temp + count);
-        if(currentDate.getMonth() == dateCount.getMonth()) {
-            appendDate(dateCount.getDate());
-            if(dateCount.getDay() == 6) {
-                addDate.addClass("saturday");
-            }
-            if(dateCount.getDay() == 0) {
-                addDate.addClass("sunday");
-            }
-        } else {
-             appendDate(dateCount.getDate(), -1);
-          }
-     }
+    var temp = previousDate - countNextDate + 1;
+    var dateCount = null;
+    var j = 0;
+    var i = 0;
+    do {
+        dateCount = new Date(year, month - 1, temp + i);
+        appendDate(dateCount);
+        if((dateCount.getDate() == lastDay.getDate()) && (lastDay.getDay() != 6)) {
+            j = 7 - lastDay.getDay();
+        }
+        if(j != 0){
+            j--;
+        }
+        i++;
+     } while((dateCount.getMonth() <= _currentDate.getMonth()) || (j != 0));
 }
 
-function appendDate(dateCount, dateCheck) {
-    var birthdayPerson = '';
-    if(dateCount != -1 && dateCheck == -1) {
-        creatingDate(dateCount);
-        date.addClass("other-month");
+function appendDate(dateCount) {
+    if(dateCount.getMonth() != _currentDate.getMonth()) {
+        creatingDate(dateCount.getDate());
+        _date.parent().addClass("other-month");
     } else {
-        creatingDate(dateCount);
-        date.click(function(event) {
-            selectRange(event.currentTarget.innerHTML);
+        creatingDate(dateCount.getDate());
+        if(dateCount.getDay() == 6) {
+            addDate.addClass("saturday");
+        }
+        if(dateCount.getDay() == 0) {
+            addDate.addClass("sunday");
+        }
+        addDate.click(function(event) {
+            selectRange(Number(event.target.childNodes[0].innerHTML));
         });
       }
-    $("#calendar").append(dateContainer);
+    $("#calendar").append(_dateContainer);
 }
 
 function selectRange(selectedDate) {
-    if(!eventStartDate) {
-        eventStartDate = selectedDate;
-        $(".date").removeClass("range-element");
-        $("#select").text('');
-    } else if(eventStartDate && !eventEndDate) {
-        eventEndDate = selectedDate;
+    var firstDayOfMonth = new Date(_currentDate.getFullYear(), _currentDate.getMonth(), 1);
+    if(_eventStartDate == 0) {
+        _eventStartDate = selectedDate;
+        $(".date-block").removeClass("range-element");
+        $(".date-block").eq(_eventStartDate + firstDayOfMonth.getDay() - 1).addClass("range-element");
+        $(".select").text('');
+    } else if(_eventStartDate != 0 && _eventEndDate == 0) {
+        _eventEndDate = selectedDate;
     }
-    if(eventStartDate && eventEndDate) {
-        var firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
-        console.log("inside both");
-        if(eventStartDate < eventEndDate){
-            while(eventStartDate <= eventEndDate) {
-                // console.log("inside inner");
-                // console.log("event occured!!" + "end" +eventEndDate);
-                // console.log($(".date-block").eq(eventStartDate + firstDayOfMonth.getDay() - 1));
-                var dayOfEvent = new Date(currentDate.getFullYear(), currentDate.getMonth(), eventStartDate);
-                $(".date-block").eq(eventStartDate + firstDayOfMonth.getDay() - 1).children().addClass("range-element");
-                $("#select").append(eventStartDate + " " + day[dayOfEvent.getDay()]);
-                eventStartDate++;
-            }
-            eventStartDate = 0;
-            eventEndDate = 0;
+    if(_eventStartDate && _eventEndDate) {
+        if(_eventStartDate < _eventEndDate){
+            colorSelectedDates(_eventStartDate, _eventEndDate, firstDayOfMonth);
         }
-        if(eventEndDate < eventStartDate){
-            while(eventEndDate <= eventStartDate) {
-                // console.log("inside inner");
-                // console.log("event occured!!" +eventStartDate+ "end" +eventEndDate);
-                // console.log($(".date-block").eq(eventEndDate + firstDayOfMonth.getDay() - 1));
-                var dayOfEvent = new Date(currentDate.getFullYear(), currentDate.getMonth(), eventEndDate);
-                $(".date-block").eq(eventEndDate + firstDayOfMonth.getDay() - 1).children().addClass("range-element");
-                $("#select").append(eventEndDate + " " + day[dayOfEvent.getDay()]);
-                eventEndDate++;
-            }
-            eventStartDate = 0;
-            eventEndDate = 0;
+        if(_eventEndDate < _eventStartDate){
+            colorSelectedDates(_eventEndDate, _eventStartDate, firstDayOfMonth);
         }
     }
+}
+
+function colorSelectedDates(startDate, endDate, firstDayOfMonth) {
+    while(startDate <= endDate) {
+        var dayOfEvent = new Date(_currentDate.getFullYear(), _currentDate.getMonth(), startDate);
+        $(".date-block").eq(startDate + firstDayOfMonth.getDay() - 1).addClass("range-element");
+        $(".select").append(startDate + " " + _day[dayOfEvent.getDay()]);
+        startDate++;
+    }
+    _eventStartDate = 0;
+    _eventEndDate = 0;
 }
 
 function creatingDate(dateCount) {
     addDate = $("<div></div>").attr("class", "date-block");
-    dateContainer.append(addDate);
-    date = $("<div></div>" , { text: dateCount }).attr("class", "date");
-    addDate.append(date);
+    _dateContainer.append(addDate);
+    _date = $("<span></span>" , { text: dateCount }).addClass("date");
+    addDate.append(_date);
 }
 
 function createMonth() {
     updateMonthHeading();
-    dateContainer.text('');
+    _dateContainer.text('');
     createCalendar();
     addBirthdayEvent();
 }
 
 function previousMonth() {
-    currentDate.setMonth(currentDate.getMonth() - 1);
+    _currentDate.setMonth(_currentDate.getMonth() - 1);
     updateYearHeading();
     createMonth();
 }
 
 function nextMonth() {
-    currentDate.setMonth(currentDate.getMonth() + 1);
+    _currentDate.setMonth(_currentDate.getMonth() + 1);
     updateYearHeading();
     createMonth();
 }
 
 function previousYear() {
-    currentDate.setFullYear(currentDate.getFullYear() - 1);
+    _currentDate.setFullYear(_currentDate.getFullYear() - 1);
     updateYearHeading();
-    dateContainer.text('');
+    _dateContainer.text('');
     createCalendar();
     addBirthdayEvent();
 }
 
 function nextYear() {
-    currentDate.setFullYear(currentDate.getFullYear() + 1);
+    _currentDate.setFullYear(_currentDate.getFullYear() + 1);
     updateYearHeading();
-    dateContainer.text('');
+    _dateContainer.text('');
     createCalendar();
     addBirthdayEvent();
 }
